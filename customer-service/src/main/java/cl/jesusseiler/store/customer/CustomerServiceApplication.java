e8dd1981e6ae8b77c2a52075ef58c702e0bfe461
seiler18 <ichbinseiler@gmail.com>
2024-09-21 13:17:44 -0300

    Finalizando Eureka Server

--- customer-service/src/main/java/cl/jesusseiler/store/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/cl/jesusseiler/store/customer/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package cl.jesusseiler.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
